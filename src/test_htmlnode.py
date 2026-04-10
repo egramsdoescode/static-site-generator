89483564b2789865acf662b3c26f415026ee71c8
egramsdoescode <ethangrams1997@gmail.com>
2026-04-10 09:50:40 -0500

    (test): Refactor HTMLNode tests

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,17 +1,17 @@
-from re import A
 import unittest
 
 from htmlnode import HTMLNode
 
 
 class TestHTMLNode(unittest.TestCase):
+    node = HTMLNode(
+        "a",
+        "test link",
+        None,
+        {"href": "https://www.google.com", "target": "_blank"},
+    )
+
     def test__repr__(self):
-        node = HTMLNode(
-            "a",
-            "test link",
-            None,
-            {"href": "https://www.google.com", "target": "_blank"},
-        )
         expected_repr = """
 tag:      a
 value:    test link
@@ -19,26 +19,13 @@ children: None
 props:    {'href': 'https://www.google.com', 'target': '_blank'}
         """
 
-        self.assertEqual(repr(node), expected_repr)
+        self.assertEqual(repr(self.node), expected_repr)
 
     def test_to_html(self):
-        node = HTMLNode(
-            "a",
-            "test link",
-            None,
-            {"href": "https://www.google.com", "target": "_blank"},
-        )
-
         with self.assertRaises(NotImplementedError):
-            node.to_html()
+            self.node.to_html()
 
     def test_props_to_html(self):
-        node = HTMLNode(
-            "a",
-            "test link",
-            None,
-            {"href": "https://www.google.com", "target": "_blank"},
-        )
         expected_props = ' href="https://www.google.com" target="_blank"'
 
-        self.assertEqual(node.props_to_html(), expected_props)
+        self.assertEqual(self.node.props_to_html(), expected_props)
